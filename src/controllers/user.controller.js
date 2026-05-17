878381998cd6f178cb13d85eb4420ecb1dd1f566
mohitydav17 <my6644866448@gmail.com>
2026-05-17 11:07:28 +0530

    added logout user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -129,8 +129,28 @@ const loginUser = asynchandler(async(req,res)=>{
 })
 
 const logoutUser = asynchandler(async(req,res)=>{
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User Logged Out"))
 })
 
 export {registerUser}
 export {loginUser}
+export {logoutUser}
